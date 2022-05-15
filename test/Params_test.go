cdd85be5408648343fbf8080fe1e6904edfa60e1
mufanx <35735090+mufanx@users.noreply.github.com>
2022-05-15 23:59:04 +0800

    tests

---
+++ test/Params_test.go
@@ -0,0 +1,12 @@
+package test
+
+import (
+	"MoTrade/OKXClient"
+	"fmt"
+	"testing"
+)
+
+func TestParamsBuilder(t *testing.T) {
+	p := OKXClient.ParamsBuilder().Set("A", "a").Set("D", "d")
+	fmt.Println(p)
+}
